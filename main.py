4a2a6fd3306c4ffc51b0305445d9db1eb5f9493c
sunshine111111 <13181711802@163.com>
2022-10-24 16:03:06 +0800

    tf_idf算法实现文档搜索

--- main.py
+++ main.py
@@ -10,12 +10,12 @@ from prepro_file import prepro_file
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
-
+#####文档数据预处理
 def Preprocess(file_url):
     PreResUrl = GrobalParament.PreprocessResultDir + '/' + GrobalParament.PreprocessResultName
     prepro_file(file_url, PreResUrl)
 
-
+####tf_idf算法实现
 def TF_IDF(*words):
     PreResUrl = GrobalParament.PreprocessResultDir + '/' + GrobalParament.PreprocessResultName
     ResFileUrl = GrobalParament.ResultFileNameDir + '/' + GrobalParament.ResultFileName
@@ -28,5 +28,6 @@ def TF_IDF(*words):
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     #Preprocess('C:/Users/zhongxing/fileSearch/test/files1')
+    ####tf-idf算法，实现关键字搜索
     TF_IDF("数据")
 
