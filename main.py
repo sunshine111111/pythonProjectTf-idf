8936ea27d4d02b4f7164362255b060178af43114
sunshine111111 <13181711802@163.com>
2022-10-25 11:28:29 +0800

    添加sunday算法查找文档关键字，并且获取关键字的索引值

--- main.py
+++ main.py
@@ -5,7 +5,7 @@
 import GrobalParament
 from TF_IDF_Compute import TF_IDF_Compute
 from prepro_file import prepro_file
-
+from sundayCompute import sundayCompute
 
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
@@ -25,9 +25,19 @@ def TF_IDF(*words):
         result = TF_IDF_Compute(PreResUrl, ResFileUrl, *words)
         return result
 
+##
+def sunday(*words):
+    PreResUrl = GrobalParament.PreprocessResultDir + '/' + GrobalParament.PreprocessResultName
+    ResFileUrl = GrobalParament.ResultFileNameDir + '/' + GrobalParament.ResultFileName
+    if GrobalParament.out_to_file:
+        sundayCompute(PreResUrl, ResFileUrl, *words)
+    else:
+        result = sundayCompute(PreResUrl, ResFileUrl, *words)
+        return result
+
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     #Preprocess('C:/Users/zhongxing/fileSearch/test/files1')
     ####tf-idf算法，实现关键字搜索
-    TF_IDF("数据")
+    sunday("数据")
 
